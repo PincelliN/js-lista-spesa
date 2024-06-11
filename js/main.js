ef979d918834627fcef2fef6dadaef9da63544e7
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-11 14:22:22 +0200

    add base project file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/*Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while */ 
+
+//creo un array
+
+const Spesa = ["mela","pera","kiwi","insalata","cipolle","zucchine","pasta","pollo","pesce-spada","cavallo","merluzzo","riso","cereali"];
